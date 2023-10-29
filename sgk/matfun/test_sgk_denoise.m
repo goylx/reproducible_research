1c17c2d974b8d6cebe703d80cbe64d044e746d4a
oylx <2236212702@qq.com>
2023-10-29 21:16:19 +0800

    change sgk

--- sgk/matfun/test_sgk_denoise.m
+++ sgk/matfun/test_sgk_denoise.m
@@ -16,77 +16,30 @@ clc;clear;close all;
 % Zu, S., H. Zhou, R. Wu, M. Jiang, and Y. Chen, 2019, Dictionary learning based on dip patch selection training for random noise attenuation, Geophysics, 84, V169?V183.
 % Zu, S., H. Zhou, R. Wu, and Y. Chen, 2019, Hybrid-sparsity constrained dictionary learning for iterative deblending of extremely noisy simultaneous-source data, IEEE Transactions on Geoscience and Remote Sensing, 57, 2249-2262.
 % etc. 
-
-%% 3D
-%% create data
-a1=zeros(300,20);
-[n,m]=size(a1);
-a3=a1;
-a4=a1;
-
-k=0;
-a=0.1;
-b=1;
-for t=-0.055:0.002:0.055
-    k=k+1;
-    b1(k)=(1-2*(pi*30*t).^2).*exp(-(pi*30*t).^2);
-    b2(k)=(1-2*(pi*40*t).^2).*exp(-(pi*40*t).^2);
-    b3(k)=(1-2*(pi*40*t).^2).*exp(-(pi*40*t).^2);
-    b4(k)=(1-2*(pi*30*t).^2).*exp(-(pi*30*t).^2);
-end
-for i=1:m
-  t1(i)=round(140);
-  t3(i)=round(-6*i+180);
-  t4(i)=round(6*i+10);
-  a1(t1(i):t1(i)+k-1,i)=b1; 
-  a3(t3(i):t3(i)+k-1,i)=b1; 
-  a4(t4(i):t4(i)+k-1,i)=b1;
-end
-
-temp=a1(1:300,:)+a3(1:300,:)+a4(1:300,:);
-for j=1:20
-    a4=zeros(300,20);
-    for i=1:m
-  t4(i)=round(6*i+10+3*j); 
-  a4(t4(i):t4(i)+k-1,i)=b1;
-  
-  t1(i)=round(140-2*j);
-  a1(t1(i):t1(i)+k-1,i)=b1;
-    end
-    shot(:,:,j)=a1(1:300,:)+a3(1:300,:)+a4(1:300,:);
-end
-plane3d=shot;
-dc=yc_scale(plane3d,3);
-dc=dc(51:225,:,:);
-figure;imagesc(reshape(dc,175,20*20));
-
-%% adding noise
-randn('state',201617);
-d=dc+0.1*randn(size(dc));
-[n1,n2]=size(d);
-figure;imagesc([dc(:,:,10),d(:,:,10)]);
-
 %% denoise using SGK %when K=3, SGK is even better than KSVD (13.23 dB)
 % the computational difference is larger when T is larger
-param=struct('T',2,'niter',10,'mode',1,'K',64);
-mode=1;l1=4;l2=4;l3=4;s1=2;s2=2;s3=2;perc=1; 
+param=struct('T',3,'niter',10,'mode',1,'K',64);
+mode=1;l1=4;l2=4;l3=4;s1=2;s2=2;s3=2;perc=1;
+d = load("C:\Users\22362\Desktop\reproducible_research\pstm_150_400_40_noise_snr0.mat").data;
+dc = load("C:\Users\22362\Desktop\reproducible_research\pstm_150_400_40_clear.mat").data;
+size(d)
 tic
 [d1,D,G,D0]=yc_sgk_denoise(d,mode,[l1,l2,l3],[s1,s2,s3],perc,param);
 toc
 yc_snr(dc,d1,2) %12.89 dB
 
-figure;imagesc([dc(:,:,10),d(:,:,10),d1(:,:,10),d(:,:,10)-d1(:,:,10)]);
+s_cplot(squeeze(d1(:,:,4)));
 
 %% benchmark with KSVD
-tic
-[d2,D2,G2,D02]=yc_ksvd_denoise(d,mode,[l1,l2,l3],[s1,s2,s3],perc,param);
-toc
-figure;imagesc([dc(:,:,10),d(:,:,10),d2(:,:,10),d(:,:,10)-d2(:,:,10)]);
+% tic
+% [d2,D2,G2,D02]=yc_ksvd_denoise(d,mode,[l1,l2,l3],[s1,s2,s3],perc,param);
+% toc
+% s_cplot(squeeze(d2(:,:,4)));
 
 %% SNR
-yc_snr(dc,d,2)  %-0.01 dB
-yc_snr(dc,d1,2) %12.89 dB
-yc_snr(dc,d2,2) %13.33 dB
+% yc_snr(dc,d,2)  %-0.01 dB
+% yc_snr(dc,d1,2) %12.89 dB
+% yc_snr(dc,d2,2) %13.33 dB
 
 
 % %% benchmark with FXYMSSA %SNR=12.77 dB
